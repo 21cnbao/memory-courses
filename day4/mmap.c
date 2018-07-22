637b27287615787a1d4d2c273cc47225b812ac0f
Barry Song <21cnbao@gmail.com>
2018-07-22 19:05:58 +0800

    add write support for mmap exam
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- day4/mmap.c
+++ day4/mmap.c
@@ -8,21 +8,25 @@ int main(int argc, char **argv)
 	char *m;
 	int i;
 
-	int fd = open("./hello.py", O_RDONLY, S_IRUSR);
+	int fd = open("./hello.py", O_RDWR, S_IRUSR);
 	if (fd == -1) {
 		perror("hello.py failed\n");
 		exit(-1);
 	};
 
 #define MAP_SIZE 50
-	m = mmap(0, MAP_SIZE, PROT_READ | PROT_EXEC,
+	m = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE,
 			MAP_SHARED, fd, 0);
 
 	if (m == MAP_FAILED) {
 		perror("mmap /dev/sda failed\n");
 		exit(-1);
 	}
+	printf("%s\n", m);
 
+	*m='a';
+	*(m+1)='b';
+	m[2]='c';
 	printf("%s\n", m);
 
 	munmap(m, MAP_SIZE);
